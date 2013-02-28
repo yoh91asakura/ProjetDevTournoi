b34a8dc0959ed18cea81960dfb11ded075455c97
Félix Girardin <girardin.felix@gmail.com>
2013-02-28 12:31:40 +0100

    Ajoute de l'ordre des Match

--- Class Php Félix/GestionTournoiFelix/GestionTournoiFelix/testFonctions.php
+++ Class Php Félix/GestionTournoiFelix/GestionTournoiFelix/testFonctions.php
@@ -1,10 +1,13 @@
 <?php
 include('Poule.class.php');
+include('GestionPoule.class.php');
 
-$tabEquipe = array ('lapin','chien','chat','tortue','chevre','girafe','rat'/*,'lezard','renard','1234','rapidStar','lapanoma','rotard','rapinotte','pluie','soleil','rapace','joviale','roger','herbert','ines','felix','rabin','kokiri','lacif','massif','klarifie','4567','ilisousous','retorque','bitokus','tokris','cloporte','pikachu','salamèche','bulbizarre','robinhood','dracofeu','polite','robespierre','volataire','rousseaux'*/);
+$tabEquipe = array ('lapin','chien','chat','tortue','chevre','girafe','rat','lezard','renard','1234','rapidStar','lapanoma','rotard','rapinotte','pluie','soleil','rapace','joviale','roger','herbert','ines','felix','rabin','kokiri','lacif','massif','klarifie','4567','ilisousous','retorque',/*'bitokus','tokris','cloporte','pikachu','salamèche','bulbizarre','robinhood','dracofeu','polite','robespierre','volataire','rousseaux'*/);
 
 $nbEquipe = count($tabEquipe);
 $poule = new Poule($tabEquipe);
+
+
 echo count($tabEquipe)."<br />";
 $poule->nbPoule(count($tabEquipe));
 echo $poule->getNbPoule()."<br />";
@@ -14,6 +17,15 @@ $poule->repartitionPoule();
 $tabPoule = $poule->getTabPoule();
 echo "<pre>";
 print_r($tabPoule);
+echo "</pre>";
+echo "<br /><br />";
+
+$gestionPoule = new GestionPoule($tabPoule);
+
+$tabMatch = $gestionPoule->genererTabMatch();
+
+echo "<pre>";
+print_r($tabMatch);
 echo "</pre>";
 
 
